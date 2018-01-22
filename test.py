b61dadb6e9f29277a02c8d507c49f81ee6f253b9
feroz5 <35457502+feroz5@users.noreply.github.com>
2018-01-22 15:07:03 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-
-with open ('book1.txt', 'r') as f:
-    for line in f:
-        row = line.split()
-        if not row:
-            continue
-        name = row[0]
-        max_score = max(map(int, row[1:]))
-        entries.append((max_score, name))
