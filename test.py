606dc2f30907866ffbe276b85379ce7cd96a9de1
feroz5 <SFerozMohamedMeeraSahib@dal.ca>
2018-01-22 14:04:34 -0500

    sdsd

---
+++ test.py
@@ -0,0 +1,11 @@
+import operator
+def descending_words():
+entries = []
+with open ('book1.txt', 'r') as f:
+    for line in f:
+        row = line.split()
+        if not row:
+            continue
+        name = row[0]
+        max_score = max(map(int, row[1:]))
+        entries.append((max_score, name))
